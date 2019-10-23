09a49c46a43cb1fa322f1114b4edacffb6b67bbc
Igor <siebtel.ss@gmail.com>
2019-10-22 21:43:51 -0300

    Colocando as tarefas 3 e 4

--- tarefa4_base/tarefa4_base.ino
+++ tarefa4_base/tarefa4_base.ino
@@ -1,11 +1,12 @@
-unsigned long int meuMillis =0;
+int ligado=1;
 void setup() {
+  pinMode(13 ,OUTPUT );
   // put your setup code here, to run once:
   TCCR1A = 0;
   TCCR1B = 0;
   TCNT1 = 0;//inicia o contador como zero
   TCCR1B |= (1 << WGM12); //aciona o CTC(clear timer on compare) 
-  OCR1A = 15626.5223;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
   //conta pra calcular esse valor 
   //[16,000,000Hz/ (prescaler * desired interrupt frequency) ] - 1
 
@@ -15,12 +16,16 @@ void setup() {
 }
 ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz toggles pin 13 (LED)
 //generates pulse wave of frequency 1Hz/2 = 0.5kHz (takes two cycles for full wave- toggle high then toggle low)
-    meuMillis++;
-    //meuMillis /= 1000;
-    Serial.println("Meu Millis");
-    Serial.println(meuMillis);
-    Serial.println("Millis");
-    Serial.println(millis());
+  if (ligado){
+    digitalWrite(13,HIGH);
+    Serial.println("high");
+    ligado = 0;
+  }
+  else{
+    digitalWrite(13,LOW);
+    Serial.println("low");
+    ligado = 1;
+  }
 }
 void loop() {
   // put your main code here, to run repeatedly:
