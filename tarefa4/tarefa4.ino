29c2d5b502fe269a26186e5cab7df4f058478271
Igor <siebtel.ss@gmail.com>
2019-10-28 16:37:39 -0300

    Desistindo da tarefa 4

--- tarefa4/tarefa4.ino
+++ tarefa4/tarefa4.ino
@@ -5,26 +5,25 @@ void setup() {
   TCCR1B = 0;
   TCNT1 = 0;//inicia o contador como zero
   TCCR1B |= (1 << WGM12); //aciona o CTC(clear timer on compare) 
-  OCR1A = 15626.5223;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
   //conta pra calcular esse valor 
   //[16,000,000Hz/ (prescaler * desired interrupt frequency) ] - 1
 
   TCCR1B |= (1 << CS12) | (1 << CS10); //aciona o prescaler de 1024
-  TIMSK1 |= (1<<OCIE1A);   // enable timer compare interrupt
+  //TIMSK1 |= (1<<OCIE1A);   // enable timer compare interrupt
   Serial.begin(9600);
 }
-ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz toggles pin 13 (LED)
-//generates pulse wave of frequency 1Hz/2 = 0.5kHz (takes two cycles for full wave- toggle high then toggle low)
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (TCNT1 & OCR1A){
     meuMillis++;
-    //meuMillis /= 1000;
     Serial.println("Meu Millis");
     Serial.println(meuMillis);
     Serial.println("Millis");
     Serial.println(millis());
-}
-void loop() {
-  // put your main code here, to run repeatedly:
-
+    TIMSK1 = 0;
+  }
 }
 
 //https://www.instructables.com/id/Arduino-Timer-Interrupts/
